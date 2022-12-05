f13c773c307a43c6261ecf5fa52e2b2a653bd070
Strawnico <nicolelicyanne0@gmail.com>
2022-12-05 11:57:16 -0300

    update

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: process.env.firebaseApiKey,
+  authDomain: "connectlove-823a8.firebaseapp.com",
+  projectId: "connectlove-823a8",
+  storageBucket: "connectlove-823a8.appspot.com",
+  messagingSenderId: "289523062458",
+  appId: "1:289523062458:web:445e5c75cad9a67d60d483",
+  measurementId: "G-GC06BD8TJF",
+};
